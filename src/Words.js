d685ffa44b4eaa9a89d519e7e96ab1238df3bff1
darenjob26 <darenjobcoladilla@gmail.com>
2022-07-20 19:53:48 +0800

    add console for bug

--- src/Words.js
+++ src/Words.js
@@ -14,9 +14,12 @@ export const generateWordSet = async () => {
     await fetch(wordBank)
         .then((response) => response.text())
         .then((result) => {
+            console.log('result', result)
             const wordArr = result.toUpperCase().split("\r\n");
+            console.log('wordArr', wordArr)
             todaysWord = wordArr[Math.floor(Math.random() * wordArr.length)];
             wordSet = new Set(wordArr);
+            console.log('wordSet', wordSet)
         });
 
     return { wordSet, todaysWord };
